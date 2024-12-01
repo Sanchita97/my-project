c19b97e0fde57448a472510787b317af2c18ba14
Sanchita97 <78336942+Sanchita97@users.noreply.github.com>
2024-12-01 14:13:40 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
+
 variable "aws_region" {
   description = "The AWS region to deploy resources"
   type        = string
